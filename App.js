9302514217bf633af87076d2271b531d45439bff
PRATEEK-MISHRA1009 <prateekmishra1009@gmail.com>
2026-03-19 17:28:21 +0530

    understaning jsx

--- App.js
+++ App.js
@@ -1,18 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React  from "react";
+import ReactDOM from "react-dom/client"
 
-const App = () => (
-  <div id="parent">
-    <div id="child">
-      <h1 key="h1-1">I am h1 tag</h1>
-      <h1 key="h1-2">I am h1 tag</h1>
-    </div>
-    <div id="child2">
-      <h1 key="h1-3">I am heading child1</h1>
-      <h2 key="h2-1">I am child heading2</h2>
-    </div>
-  </div>
-)
+// this is jsx-----html or xml like syntax
+const jsxHeading=<h1 id="heading">Namaste React from heading</h1>
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading)
